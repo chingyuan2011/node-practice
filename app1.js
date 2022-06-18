24f769440bc74341999f69ee12d1fddc0a83207a
ryan.yang <ryan.yang@104.com.tw>
2022-06-18 17:03:10 +0800

    require / module.exports / exports

--- app1.js
+++ app1.js
@@ -1,1 +1,6 @@
-global.a = 1
+var content = require('./app2')
+var a = 1
+
+console.log(a)
+console.log(content.data)
+console.log(content.bark())
--- app2.js
+++ app2.js
@@ -1,2 +1,13 @@
-global.b = 1
-console.log(global)
+exports.data = 2
+
+exports.bark = function () {
+    return 'Bark!!!'
+}
+
+// 不能與 exports.data 共用，會把 bark 的值蓋掉
+module.exports = {
+    data: 3,
+    bark: () => {
+        return 'Bark2!!!'
+    }
+}
