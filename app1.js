bc815c1db2b298985a909da1a10b12509e42ed9c
ryan.yang <ryan.yang@104.com.tw>
2022-06-20 22:29:45 +0800

    dirname, filename

--- app1.js
+++ app1.js
@@ -1,16 +1,8 @@
-var http = require('http')
+var path = require('path')
+var a = 1
 
-// request: 對方要求申請進到這個 server使用者讀取到網站，會收到的資料
-// response: 收到資料後要回傳的資料
-http.createServer(function(request, response){
-    // 表頭內容 - 200 傳送成功
-    // Content-Type 回傳文字給網頁 text/plain； text/html
-    response.writeHead(200, {"Content-Type": "text/plain"})
-    response.write("hello~")
-    // 結束回應
-    response.end()
+var b = 2
+console.log(__dirname); // 資料夾位置
+console.log(__filename); // 目前執行的檔案位置
 
-    // 監聽 port
-}).listen(8080)
-
-// 可使用 127.0.0.1:8080 看到畫面
+var c = 3
