1d54cee2cba46fb120fec43e7deffe4a495801ad
ryan.yang <ryan.yang@104.com.tw>
2022-09-06 21:24:09 +0800

    [學習] GET/DELETE 頁面設置

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const http = require('http')
 // req 請求者資料
 // res 回傳資料
 const requestListener = ((req, res) => {
-    console.log(req.url, req.method);
-    res.writeHead(200, {"Content-Type":"text/plain"})
-    res.write('hello')
-    res.end()
+    const headers = {
+        "Content-Type":"text/plain"
+    }
+
+    if(req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, headers)
+        res.write('GET!')
+        res.end()
+    } else if (req.url === '/' && req.method === 'DELETE') {
+        res.writeHead(200, headers)
+        res.write('DELETE!')
+        res.end()
+    } else {
+        res.writeHead(404, headers)
+        res.write('ERROR!')
+        res.end()
+    }
+    
 })
 
 const server = http.createServer(requestListener)
