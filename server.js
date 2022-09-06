ca715c117e857e46d0e67dc64039ff8b5213c949
ryan.yang <ryan.yang@104.com.tw>
2022-09-06 21:52:24 +0800

    [學習] preflight 機制

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const requestListener = ((req, res) => {
             "data": []
         }))
         res.end()
+    } else if(req.url === '/' && req.method === 'OPTIONS') {
+        // preflight 機制
+        res.writeHead(200, headers)
+        res.end()
     } else {
         res.writeHead(404, headers)
         res.write(JSON.stringify({
