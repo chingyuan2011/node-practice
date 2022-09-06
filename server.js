369a42f71bb139cf823335eab99bd8650035aa0b
ryan.yang <ryan.yang@104.com.tw>
2022-09-06 21:34:21 +0800

    [學習] 修改表頭、回傳 json 格式

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const http = require('http')
 // res 回傳資料
 const requestListener = ((req, res) => {
     const headers = {
-        "Content-Type":"text/plain"
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "PATCH, GET, POST, OPTIONS, DELETE",
+        "Content-Type": "application/json"
     }
 
     if(req.url === '/' && req.method === 'GET') {
         res.writeHead(200, headers)
-        res.write('GET!')
-        res.end()
-    } else if (req.url === '/' && req.method === 'DELETE') {
-        res.writeHead(200, headers)
-        res.write('DELETE!')
+        // 網路封包只能解析字串
+        res.write(JSON.stringify({
+            "status": "success",
+            "data": []
+        }))
         res.end()
     } else {
         res.writeHead(404, headers)
-        res.write('ERROR!')
+        res.write(JSON.stringify({
+            "status": "error",
+            "message": "錯誤路由"
+        }))
         res.end()
     }
     
