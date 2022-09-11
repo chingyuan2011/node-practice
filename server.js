27fcf9bf036276ab1b9302516449250a97b1ea49
ryan.yang <ryan.yang@104.com.tw>
2022-09-11 14:00:26 +0800

    [學習] 接收 POST API 的 body 資料

--- server.js
+++ server.js
@@ -1,38 +1,72 @@
-const http = require('http')
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+
+const todos = [
+  {
+    title: "工作內容1",
+    id: uuidv4(),
+  },
+];
 
 // req 請求者資料
 // res 回傳資料
-const requestListener = ((req, res) => {
-    const headers = {
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "PATCH, GET, POST, OPTIONS, DELETE",
-        "Content-Type": "application/json"
-    }
+const requestListener = (req, res) => {
+  const headers = {
+    "Access-Control-Allow-Headers":
+      "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PATCH, GET, POST, OPTIONS, DELETE",
+    "Content-Type": "application/json",
+  };
 
-    if(req.url === '/' && req.method === 'GET') {
-        res.writeHead(200, headers)
-        // 網路封包只能解析字串
-        res.write(JSON.stringify({
-            "status": "success",
-            "data": []
-        }))
-        res.end()
-    } else if(req.url === '/' && req.method === 'OPTIONS') {
-        // preflight 機制
-        res.writeHead(200, headers)
-        res.end()
-    } else {
-        res.writeHead(404, headers)
-        res.write(JSON.stringify({
-            "status": "error",
-            "message": "錯誤路由"
-        }))
-        res.end()
-    }
-    
-})
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
 
-const server = http.createServer(requestListener)
-server.listen(3005)
+  if (req.url === "/todos" && req.method === "GET") {
+    res.writeHead(200, headers);
+    // 網路封包只能解析字串
+    res.write(
+      JSON.stringify({
+        status: "success",
+        data: todos,
+      })
+    );
+    res.end();
+  } else if (req.url === "/todos" && req.method === "POST") {
+    // 取得 request 中的 body
+    req.on("end", () => {
+      const todo = {
+        title: JSON.parse(data).title,
+        id: uuidv4(),
+      };
+      todos.push(todo);
 
+      res.writeHead(200, headers);
+      res.write(
+        JSON.stringify({
+          status: "success",
+          data: todos,
+        })
+      );
+      res.end();
+    });
+  } else if (req.url === "/todos" && req.method === "OPTIONS") {
+    // preflight 機制 - 跨網域
+    res.writeHead(200, headers);
+    res.end();
+  } else {
+    res.writeHead(404, headers);
+    res.write(
+      JSON.stringify({
+        "status": "error",
+        "message": "錯誤路由",
+      })
+    );
+    res.end();
+  }
+};
+
+const server = http.createServer(requestListener);
+server.listen(3005);
