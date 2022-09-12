08d694d0532d100b0b591d5c73fe5ce5ce42f3e9
ryan.yang <ryan.yang@104.com.tw>
2022-09-12 23:21:57 +0800

    [學習] promise 練習：基本、帶入參數、多組 promise 組裝

---
+++ server.js
@@ -0,0 +1,77 @@
+// 基本 promise
+// function checkScore = new Promise((resolve, reject) => {
+//   console.log("正在批改中");
+//   setTimeout(() => {
+//     const score = Math.round(Math.random() * 100);
+//     if (score >= 60) {
+//       resolve(score);
+//     } else {
+//       reject("不及格");
+//     }
+//   }, 1000);
+// });
+// checkScore.then((data) => console.log(data)).catch((err) => console.log(err));
+
+// promise 帶入參數
+// function checkScore2(name){
+//   return new Promise((resolve, reject) => {
+//     console.log("作業二正在批改中");
+//     setTimeout(() => {
+//       const score = Math.round(Math.random() * 100);
+//       if (score >= 60) {
+//         resolve(name + score + "分");
+//       } else {
+//         reject(name + "不及格");
+//       }
+//     }, 1000);
+//   });
+// };
+
+// checkScore2("小明")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// 組裝多個 promise
+function correctTest3(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const score = Math.round(Math.random() * 100);
+      if (score >= 50) {
+        resolve({
+          name,
+          score,
+        });
+      } else {
+        reject({
+          name,
+          score,
+        });
+      }
+    });
+  }, 2000);
+}
+
+function checkAward3 (data) {
+    return new Promise((resolve, reject) => {
+        console.log('正在檢查獎品中...');
+        setTimeout(() => {
+            if(data.score > 70) {
+                resolve({
+                    ...data,
+                    award: '1000元'
+                })
+            } else {
+                reject({
+                    ...data,
+                    award: '500元'
+                })
+            }
+        }, 1000);
+
+    })
+}
+
+correctTest3("小明")
+  .then((data) => checkAward3(data))
+  .then((data) => console.log(data))
+  .catch((err) => console.log('err', err));
