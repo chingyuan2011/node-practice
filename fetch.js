4ba6ca569e31d0464e9a470b2e2709d425a53d26
ryan.yang <ryan.yang@104.com.tw>
2022-09-26 21:46:19 +0800

    [學習] 手刻 axios

--- fetch.js
+++ fetch.js
@@ -1,21 +1,21 @@
-console.log('fetch');
+const url = 'https://raw.githubusercontent.com/hexschool/2021-ui-frontend-job/master/frontend_data.json'
 
 
+// 1. fetch
 // fetch(url)
 //     .then(res => res.json())
 //     .then(data => console.log('res', data))
 //     .catch(err => console.log('err', err))
 
 
-// XMLHttpRequest
-const xhr = new XMLHttpRequest();
-xhr.open("GET", url)
-xhr.onload = () => { console.log('xhr', xhr.responseText) }
-xhr.onerror = () => { console.log('xhr err', xhr.statusText) }
-xhr.send()
+// 2. XMLHttpRequest
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", url)
+// xhr.onload = () => { console.log('xhr', xhr.responseText) }
+// xhr.onerror = () => { console.log('err',xhr.statusText) }
+// xhr.send()
 
-
-// 結合 Promise
+// 3. XMLHttpRequest
 // function getUrl (url) {
 //  return new Promise((resolve, reject) => {
 //     const xhr = new XMLHttpRequest();
@@ -29,3 +29,20 @@ xhr.send()
 // getUrl(url)
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err))
+
+// 4. 手刻 axios
+const axios = {
+    get: function(url){
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url)
+            xhr.onload = () => resolve(xhr.responseText)
+            xhr.onerror = () => reject(xhr.statusText)
+            xhr.send()
+        })
+    }
+}
+
+axios.get(url)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
